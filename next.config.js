775c72f8f15e41ab430e513bfe1d93d32cb8abfe
buyyabuyyabuyya <buyyav1@gmail.com>
2025-06-06 19:16:12 -0700

    this is the update for the worker broweerless URL update caller  2

--- next.config.js
+++ next.config.js
@@ -10,7 +10,19 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve('stream-browserify'),
+        querystring: require.resolve('querystring-es3'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+      };
+    }
+    return config;
+  },
 
 };
 
